a618be7f852f1dc20dd067a1dc491a3441603ee9
krittidet-prompet <krittidet.prompet@gmail.com>
2026-05-27 19:25:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 window.KAPI_CONFIG = {
   // 1) ใส่ URL ที่ได้จาก Apps Script Web App Deployment ต้องลงท้ายด้วย /exec
-  GAS_API_URL: 'PUT_YOUR_APPS_SCRIPT_WEB_APP_EXEC_URL_HERE',
+  GAS_API_URL: 'https://script.google.com/macros/s/AKfycbye1DJNo8WW9AgqhwBc28vldZvhWWPeWih1TfaU-D1dAqQqZDqs5m3Ku5WJ4bQIKTaMDA/exec',
 
   // 2) ต้องตรงกับ Script Property: API_PUBLIC_TOKEN ใน Apps Script
-  API_PUBLIC_TOKEN: 'PUT_YOUR_API_PUBLIC_TOKEN_HERE',
+  API_PUBLIC_TOKEN: 'kapi-maedang-github1212312121',
 
   APP_VERSION: '3.1.0'
 };
